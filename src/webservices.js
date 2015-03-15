09a222fdb37e279e5d691a4b83893313f78bd277
Christophe Benz <christophe.benz@data.gouv.fr>
2015-03-15 14:01:38 +0100

    Fix API URL

--- src/webservices.js
+++ src/webservices.js
@@ -27,7 +27,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 'use strict';
 
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL = 'http://localhost:3000/api/1';
 
 
 // HTTP functions
