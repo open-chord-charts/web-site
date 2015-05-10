247c7a620a630cc9019cb7e6f3da90dbcb4b8d82
Christophe Benz <christophe.benz@data.gouv.fr>
2015-05-10 13:02:30 +0200

    Handle not found routes

--- src/server/render.jsx
+++ src/server/render.jsx
@@ -9,20 +9,9 @@ import {fetchData, routes} from "../routes";
 
 
 const debug = require("debug")("app:render");
-let webpackStats;
-
-if (process.env.NODE_ENV === "production") {
-  webpackStats = require("./webpack-stats.json");
-}
 
 
 function render(req, res, next) {
-  if (process.env.NODE_ENV === "development") {
-    webpackStats = require("./webpack-stats.json");
-    // Do not cache webpack stats: the script file would change since
-    // hot module replacement is enabled in the development env
-    delete require.cache[require.resolve("./webpack-stats.json")];
-  }
   var router = Router.create({
     location: req.url,
     onAbort(redirect) {
@@ -35,21 +24,40 @@ function render(req, res, next) {
     routes: routes,
   });
   router.run((Root, state) => {
+    if (state.routes.some(route => route.isNotFound)) {
+      const appHtml = React.renderToString(<Root/>);
+      res.status(404).send(renderMarkup(appHtml));
+      return;
+    }
     fetchData(state.routes, state.params, state.query)
       .then(
         data => React.renderToString(<Root loading={false} {...data} />),
         errorByRouteName => React.renderToString(<Root errorByRouteName={errorByRouteName} loading={false} />)
-      ).then(markup => {
-        const css = webpackStats.css.concat([
-          process.env.NODE_ENV === "production" ? "/assets/basscss.min.css" : "/assets/basscss.css",
-        ]);
-        const html = React.renderToStaticMarkup(
-          <HtmlDocument css={css} markup={markup} scripts={webpackStats.script} />
-        );
-        const doctype = "<!DOCTYPE html>";
-        res.send(doctype + html);
-      }).catch(error => next(error));
+      )
+      .then(appHtml => res.send(renderMarkup(appHtml)))
+      .catch(error => next(error));
   });
+}
+
+
+function renderMarkup(appHtml) {
+  let webpackStats;
+  if (process.env.NODE_ENV === "production") {
+    webpackStats = require("./webpack-stats.json");
+  } else if (process.env.NODE_ENV === "development") {
+    webpackStats = require("./webpack-stats.json");
+    // Do not cache webpack stats: the script file would change since
+    // hot module replacement is enabled in the development env
+    delete require.cache[require.resolve("./webpack-stats.json")];
+  }
+  const doctype = "<!DOCTYPE html>";
+  const css = webpackStats.css.concat([
+    process.env.NODE_ENV === "production" ? "/assets/basscss.min.css" : "/assets/basscss.css",
+  ]);
+  const html = React.renderToStaticMarkup(
+    <HtmlDocument appHtml={appHtml} css={css} scripts={webpackStats.script} />
+  );
+  return doctype + html;
 }
 
 
